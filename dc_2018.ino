6e8c74fc394eb3461b252da80ef3161f34a003e5
Wyatt Cook <wcookeverton@gmail.com>
2018-04-25 16:57:10 -0500

    2018 Init

---
+++ dc_2018.ino
@@ -0,0 +1,16 @@
+/*
+ * Code is for Design Competition 2018
+ * All code and strategy written by
+ * Josh Cohen, Wyatt Cook, and The. Daniel Sours
+ */
+
+ 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
