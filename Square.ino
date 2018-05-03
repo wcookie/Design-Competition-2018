6d40c645dc55b54bb112d0b77ddd7dcfabb9f1a8
Wyatt Cook <wcookeverton@gmail.com>
2018-05-03 18:35:20 -0500

    filled out initialization of all squares functions

--- Square.ino
+++ Square.ino
@@ -1,5 +1,17 @@
 //Square comes before in arguments
 
+Square Squares[] = {Square(Point(1.0, 2.0), 5.0),
+                    Square(Point(5.0, -4.0), 5.0),
+                    Square(Point(2.0, 0.0), 5.0)
+};
+
+Square findNearestSquare(Point p) {
+  /*
+   * given a point, go through the squares array and 
+   * find the nearest square goal to go to
+   */
+}
+
 Point findNearestEdge(Square s, Point p) {
   /*
    * Given a square and our robot's pont, finds the nearest point
