b9fc755b5cef8993c6a85f15a9f2a94c3df17629
Wyatt Cook <wcookeverton@gmail.com>
2018-05-08 17:56:21 -0500

    Also put in first draft of our virtual to physical coordinate plane code

--- Point.ino
+++ Point.ino
@@ -13,29 +13,47 @@ double Y_COORD_RANGE = 60;
  * Left/right /up and down is defined from looking from the Design Competition sign perspective
  */
 
- LightPoint bottomLeftCorner = LightPoint(-4.0, -2.4); 
- LightPoint bottomRightCorner = LightPoint(-4.0, 2.4);
- LightPoint topLeftCorner = LightPoint(4.0, -2.4);
- LightPoint topRightCorner = LightPoint(4.0, 2.4);
+LightPoint bottomLeftCorner = LightPoint(-4.0, -2.4); 
+LightPoint bottomRightCorner = LightPoint(-4.0, 2.4);
+LightPoint topLeftCorner = LightPoint(4.0, -2.4);
+LightPoint topRightCorner = LightPoint(4.0, 2.4);
 
 
- Point physicalPointToVirtualPoint(LightPoint lp) {
+Point physicalPointToVirtualPoint(LightPoint lp) {
+ /*
+  * turns a physical point into our virtual coordinate plane.
+  * calculates the physical ranges for x and Y and uses the proportion of the range verse
+  * our virtual range.  Then throws that proportion onto our individual point.
+  * Maybe TODO: Make some of this a setup task that affects global variables
+  */
+ double leftX = (double)(bottomLeftCorner.x + topLeftCorner.x) / 2.0;
+ double rightX = (double)(bottomRightCorner.x + topRightCorner.x) / 2.0;
+ double topY = (double)(topLeftCorner.y + topRightCorner.y) / 2.0;
+ double bottomY = (double)(bottomLeftCorner.y + bottomRightCorner.y) / 2.0;
+ double xDiff = rightX - leftX;
+ double yDiff = topY - bottomY;
+ double xProp = X_COORD_RANGE / xDiff;
+ double yProp = Y_COORD_RANGE / yDiff;
+ double virtualX = xProp * lp.x;
+ double virtualY = yProp * lp.y;
+ return Point(virtualX, virtualY);
+}
+
+LightPoint virtualPointToPhysicalPoint(Point p) {
   /*
-   * turns a physical point into our virtual coordinate plane.
-   * calculates the physical ranges for x and Y and uses the proportion of the range verse
-   * our virtual range.  Then throws that proportion onto our individual point.
-   * Maybe TODO: Make some of this a setup task that affects global variables
+   *The inverse of the above function, gets a physical point for us from a virtual one. 
    */
-  double leftX = (double)(bottomLeftCorner.x + topLeftCorner.x) / 2.0;
-  double rightX = (double)(bottomRightCorner.x + topRightCorner.x) / 2.0;
-  double topY = (double)(topLeftCorner.y + topRightCorner.y) / 2.0;
-  double bottomY = (double)(bottomLeftCorner.y + bottomRightCorner.y) / 2.0;
-  double xDiff = rightX - leftX;
-  double yDiff = topY - bottomY;
-  double xProp = X_COORD_RANGE / xDiff;
-  double yProp = Y_COORD_RANGE / yDiff;
-  double virtualX = xProp * lp.x;
-  double virtualY = yProp * lp.y;
-  return Point(virtualX, virtualY);
- }
+ double leftX = (double)(bottomLeftCorner.x + topLeftCorner.x) / 2.0;
+ double rightX = (double)(bottomRightCorner.x + topRightCorner.x) / 2.0;
+ double topY = (double)(topLeftCorner.y + topRightCorner.y) / 2.0;
+ double bottomY = (double)(bottomLeftCorner.y + bottomRightCorner.y) / 2.0;
+ double xDiff = rightX - leftX;
+ double yDiff = topY - bottomY;
+ double xProp = X_COORD_RANGE / xDiff;
+ double yProp = Y_COORD_RANGE / yDiff;
+ double physicalX = p.x / xProp;
+ double physicalY = p.y / yProp;
+ return LightPoint(physicalX, physicalY);
+}
+
 
